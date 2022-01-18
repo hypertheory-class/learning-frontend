35d094b226609c200e3ebf0de64fa32957c70829
Jeffry Gonzalez <jeff@hypertheory.com>
2022-01-18 13:57:17 -0500

    started functions

--- src/learning/functions.spec.ts
+++ src/learning/functions.spec.ts
@@ -1,7 +1,28 @@
 describe('functions', () => {
-  describe('declaring functions', () => {});
+  describe('declaring functions', () => {
+    it('named functions', () => {
+      //  int Add(int a, int b) { ... }
+      function add(a: number, b: number): number {
+        return a + b;
+      }
+
+      expect(add(2, 2)).toBe(4);
+      expect(subtract(10, 2)).toBe(8);
+      expect(subtract(20, 3)).toBe(17);
+    });
+
+    it('anonymous functions', () => {
+      expect(subtract(10, 2)).toBe(8);
+    });
+  });
 
   describe('higher order functions', () => {});
 
   describe('array methods', () => {});
 });
+
+// files in typescript and javascript are "modules" (sort of. more later)
+// "Named" - they can be 'forward referenced'
+function subtract(a: number, b: number): number {
+  return a - b;
+}
