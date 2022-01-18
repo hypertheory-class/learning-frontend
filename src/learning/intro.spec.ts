e930b92aa97595388ad10f4e8379510a5ae5262c
Jeffry Gonzalez <jeff@hypertheory.com>
2022-01-18 11:44:40 -0500

    beginning tests

---
+++ src/learning/intro.spec.ts
@@ -0,0 +1,12 @@
+describe('data types in typescript', () => {
+  describe('literal types', () => {
+    it('basic test', () => {
+      let a = 10;
+      let b = 20;
+
+      let answer = a + b;
+
+      expect(answer).toBe(30);
+    });
+  });
+});
