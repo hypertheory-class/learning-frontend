cb2c9c0e5c7f10a3824d1610c9ff88e833844c70
Jeffry Gonzalez <jeff@hypertheory.com>
2022-01-18 10:40:42 -0500

    cleaned up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning Resources';
-
-  topics = [
-    'What is Angular?',
-    'TypeScript 101',
-    'Building Components',
-    'State Management',
-  ];
-
-  makeUpper() {
-    this.title = this.title.toUpperCase();
-  }
-
-  addItem(what: HTMLInputElement) {
-    this.topics = [what.value, ...this.topics];
-    what.value = '';
-    what.focus();
-  }
 }
