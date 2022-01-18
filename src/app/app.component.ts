83c75521f44126d00f49a4ffdbc602279e0f4e63
Jeffry Gonzalez <jeff@hypertheory.com>
2022-01-18 10:38:01 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'learning-frontend';
+  title = 'Learning Resources';
+
+  topics = [
+    'What is Angular?',
+    'TypeScript 101',
+    'Building Components',
+    'State Management',
+  ];
+
+  makeUpper() {
+    this.title = this.title.toUpperCase();
+  }
+
+  addItem(what: HTMLInputElement) {
+    this.topics = [what.value, ...this.topics];
+    what.value = '';
+    what.focus();
+  }
 }
