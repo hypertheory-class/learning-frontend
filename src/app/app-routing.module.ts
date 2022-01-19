27f27c146a33011ad0f4ade651511cdd1cc22011
Jeffry Gonzalez <jeff@hypertheory.com>
2022-01-19 11:45:33 -0500

    set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'thank-you',
+    component: ThankYouComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TitleBarComponent, DashboardComponent, ThankYouComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [], // services!
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
